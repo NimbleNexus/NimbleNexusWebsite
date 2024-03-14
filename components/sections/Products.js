8a6b695d4aac823aacde15c1aa32ec70358e8a01
Mihai Bîrsan <alexandru.m.birsan@gmail.com>
2024-03-14 16:50:52 +0200

    Products and Services

---
+++ components/sections/Products.js
@@ -0,0 +1,85 @@
+
+export default function Features13_3() {
+    return (
+        <>
+            <section id="features-13" className="shape--bg shape--white-500 pt-100 features-section division">
+                <div className="container">
+                    <div className="row d-flex align-items-center">
+                        {/* FEATURES-13 WRAPPER */}
+                        <div className="col-md-7 order-last order-md-2">
+                            <div className="fbox-13-wrapper">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        {/* FEATURE BOX #1 */}
+                                        <div id="fb-12-1" className="fbox-12 bg--white-100 block-shadow r-12 mb-30">
+                                            {/* Icon */}
+                                            <div className="fbox-ico ico-50">
+                                                <div className="shape-ico color--theme">
+                                                    {/* Vector Icon */}
+                                                    <span className="flaticon-layers-1" />
+                                                    {/* Shape */}
+                                                    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
+                                                    </svg>
+                                                </div>
+                                            </div>	{/* End Icon */}
+                                            {/* Text */}
+                                            <div className="fbox-txt">
+                                                <h5 className="s-19 w-700">Portrait Generator</h5>
+                                                <p>AI artistry at your fingertips</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        {/* FEATURE BOX #3 */}
+                                        <div id="fb-12-3" className="fbox-12 bg--white-100 block-shadow r-12 mb-30">
+                                            {/* Icon */}
+                                            <div className="fbox-ico ico-50">
+                                                <div className="shape-ico color--theme">
+                                                    {/* Vector Icon */}
+                                                    <span className="flaticon-pay-per-click" />
+                                                    {/* Shape */}
+                                                    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
+                                                    </svg>
+                                                </div>
+                                            </div>	{/* End Icon */}
+                                            {/* Text */}
+                                            <div className="fbox-txt">
+                                                <h5 className="s-19 w-700">Omoikane</h5>
+                                                <p>An affordable AI assistant, crafted to take the tedious out of your to-do list</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>	{/* End row */}
+                        </div>	{/* END FEATURES-13 WRAPPER */}
+                        {/* TEXT BLOCK */}
+                        <div className="col-md-5 order-first order-md-2">
+                            <div className="txt-block left-column">
+                                {/* Section ID */}
+                                <span className="section-id">Products and Services</span>
+                                {/* Title */}
+                                <h2 className="s-46 w-700">What we build</h2>
+                                {/* Text */}
+                                <p>Technology to harness the power of AI and put it in the service of humanity.</p>
+                                {/* List */}
+                                <ul className="simple-list">
+                                    <li className="list-item">
+                                        <p className="mb-0">Time-saving Technology</p>
+                                    </li>
+                                    <li className="list-item">
+                                        <p className="mb-0">User-centric Design</p>
+                                    </li>
+                                    <li className="list-item">
+                                        <p className="mb-0">Innovative Solutions for Everyday Challenges</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>	{/* END TEXT BLOCK */}
+                    </div>    {/* End row */}
+                </div>     {/* End container */}
+            </section>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Ct02_14 from "../components/sections/Ct02_14"
 import Ct04_3 from "../components/sections/Ct04_3"
 import Ct08_3 from "../components/sections/Ct08_3"
 import Faqs3_6 from "../components/sections/Faqs3_6"
-import Features13_3 from "../components/sections/Features13_3"
 import Features2_7 from "../components/sections/Features2_7"
 import Integrations2_4 from "../components/sections/Integrations2_4"
 import Lnk1_7 from "../components/sections/Lnk1_7"
@@ -17,6 +16,7 @@ import Wswrapper_1 from "../components/sections/Wswrapper_1"
 import Banner7_2 from "../components/sections/Contact"
 import Hero7 from "../components/sections/Hero"
 import Features6_3 from "../components/sections/HowItWorks"
+import Features13_3 from "../components/sections/Products"
 export default function Home() {
 
     return (
